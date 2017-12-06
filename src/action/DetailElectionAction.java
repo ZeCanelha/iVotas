9f842f3ff233a4f5f82f0f70ef14fe982e632803
Jose miguel Mendes Canelha <josemc@student.dei.uc.pt>
2017-12-06 18:40:56 +0000

    Na consola de admin fica a faltar saber onde o user votou É preciso formatar o display das eleicoes. show.jsp

---
+++ src/action/DetailElectionAction.java
@@ -0,0 +1,62 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import model.DetailElectionBean;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DetailElectionAction extends ActionSupport implements SessionAware {
+	private static final long serialVersionUID = 4L;
+	private Map<String, Object> session;
+	private String ideleicao;
+
+	
+
+	@Override
+	public String execute() {
+		
+		HashMap<String,String> map = null;
+		this.getDetailBean().setIdeleicao(ideleicao);
+		try {
+			map = this.getDetailBean().getDetailElections();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (map.get("title") != null)
+			return SUCCESS;
+		return ERROR;
+		
+		
+			
+	}
+
+
+	public DetailElectionBean getDetailBean() {
+		if(!session.containsKey("detailBean"))
+			this.setDetailBean(new DetailElectionBean());
+		return (DetailElectionBean) session.get("detailBean");
+	}
+
+	public void setDetailBean(DetailElectionBean detailBean) {
+		this.session.put("detailBean", detailBean);
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+	
+	public String getIdeleicao() {
+		return ideleicao;
+	}
+	public void setIdeleicao(String ideleicao) {
+		this.ideleicao = ideleicao;
+	}
+}
---
+++ src/model/DetailElectionBean.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+import Interface.RMIInterface;
+
+public class DetailElectionBean {
+	private RMIInterface server;
+	private String ideleicao;
+	
+	private static final String rmiAddress = "rmi://127.0.0.1:6005/rmiserver";
+	
+
+	public DetailElectionBean() {
+		
+		try {
+			server = (RMIInterface) Naming.lookup(rmiAddress);
+		}
+		catch(NotBoundException|MalformedURLException|RemoteException e) {
+
+			e.printStackTrace();
+		}
+	}
+	
+	public HashMap<String,String> getDetailElections() throws RemoteException
+	{
+		return server.detail_election(ideleicao);
+	}
+
+
+	public String getIdeleicao() {
+		return ideleicao;
+	}
+
+
+	public void setIdeleicao(String ideleicao) {
+		this.ideleicao = ideleicao;
+	}
+	
+
+	
+
+}
